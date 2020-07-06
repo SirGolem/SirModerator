ba50a4f22e989853a07a78e3e1e6850793e94965
Glitch (hello-express) <none>
2020-07-06 15:22:58 +0000

    😬🏄 Checkpoint ./bot/main/index.js:18570975/242

--- bot/main/index.js
+++ bot/main/index.js
@@ -4,4 +4,10 @@ const client = new Discord.Client()
 
 const db = require('quick.db')
 
-const MessageEvent = require('')
+const MessageEvent = require('../events/message.js'),
+      ReadyEvent = require('../events/ready.js'),
+      LoadCommands = require('..handlers/commands.js')
+
+client.commands = new Discord.Collection()
+client.owners = ["562303831040327680",""]
+client.
