916b414445d91963ed5da94cdffe0e32737b3268
Glitch (hello-express) <none>
2020-07-06 16:20:44 +0000

    👾🎄 Checkpoint ./bot/handlers/commands.js:18570975/184 ./bot/events/ready.js:18570975/38

--- bot/handlers/commands.js
+++ bot/handlers/commands.js
@@ -3,6 +3,11 @@ const fs = require('fs')
 const event = client => {
   const folders = fs.readirSync(".bot/commands/")
 for (const folder of folders) {
-  const files = fs.readirSync(".bot/commands/"+ )
+  const files = fs.readirSync(".bot/commands/" + folder + "/")
+  for (const commands of files) {
+    const command = require(`../commands${folder}/${commands}`)
+    client.commands.set(command.name, command)
+  }
 }
   }
+module.export = event
