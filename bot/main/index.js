e3024f6870025f57301cc3e1615b9551566cb1cc
Glitch (hello-express) <none>
2020-07-06 15:45:49 +0000

    🚈🍍 Checkpoint ./bot/main/index.js:18570975/1 ./bot/handlers/commands.js:18570975/62

--- bot/main/index.js
+++ bot/main/index.js
@@ -18,4 +18,4 @@ client.on("ready", () => ReadyEvent(client,db))
 
 client.on("message", (message) => MessageEvent(client, message, db))
 
-clien.login("NzE5NjE4MDQzNTM3NTIyODEw.XwNDaA.6ErnXQvFOFHaGZ6w88t-ALq0ycw")
+client.login("NzE5NjE4MDQzNTM3NTIyODEw.XwNDaA.6ErnXQvFOFHaGZ6w88t-ALq0ycw")
